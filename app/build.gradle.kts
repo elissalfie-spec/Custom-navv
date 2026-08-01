1806a306010455e871122c5d9adf4cfa6d37129c
elissalfie-spec <elissalfie@gmail.com>
2026-08-01 15:31:13 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.navapp"
+    namespace = "com.example.customnav"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.navapp"
-        minSdk = 26
+        applicationId = "com.example.customnav"
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -18,10 +18,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
@@ -31,22 +27,7 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
-    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
 }
